ccbd66148d0d2fc55ec4522b2a695a3fd60df6a0
diego <dmdalera@gmail.com>
2023-05-18 11:05:41 -0300

    add login

--- js/carga.js
+++ js/carga.js
@@ -1,93 +1,320 @@
 
-saludar();
-// La f(x) saludar() el la que dispara todo el proceso, originalmente se llamaba al cargar la pagina .html pero se saco a pedido del tutor
-function saludar() {
-    let nombre = prompt("Ingresa tu Nombre de pila por favor ");
-    let apellido = prompt("Ingresa tu Apellido  por favor ");
+//EventListener Index.Html
 
-    if (nombre.trim() == "" || apellido.trim() == "") {
-        alert("por favor ingresa por lo menos un caracter en cada respuesta");
-        alert("Comenza nuevamente ");
-        saludar();
-    }
+const propiedadesCardsPromociones = document.querySelector(".container");
+const propiedadesCardsPrincipal = document.querySelector(".container");
+const propiedadesCardsUltimosIngresos = document.querySelector(".container_ultimas_propiedades");
+const formularioBusquedaPropiedades = document.getElementById("formulario_busqueda_propiedades");
 
-    alert("Bienvenido a nuestra inmobiliaria: " + nombre + " - " + apellido);
+//EventListener CRUD 
 
-    let opcion = prompt("Deseas continuar con esta presentacion (s/n)");
+const tablaDePropiedades = document.getElementById("tabla_show")
 
-    if (opcion.trim() == 's' || opcion.trim() == 'S') {
-        logeo(apellido)
+// Carga de propiedades y options en el index.html al inicio 
+
+function cargaInicio() {
+  cargaPropiedadesPromocionadas();
+  cargarOpcionesBusqueda();
+  cargarOpcionesBusquedaTest();
+  cargaUltimasPropiedades();
+  //mostrarModal();
+}
+
+function cargarTablaCrud() {
+
+  let code = "<table class='fl-table'>";
+
+  propiedadesArray.forEach(function (propiedades) {
+
+    code = code +
+       `
+      <tr>
+        <td>${propiedades.title}</td>
+        <td>${propiedades.descripcion}</td>
+        <td>${propiedades.bedrooms}</td>
+        <td>${propiedades.bathrooms}</td>
+        <td>${propiedades.area}</td>
+        <td>${propiedades.price}</td>
+        <td>${propiedades.type}</td>
+        <td>${propiedades.zone}</td>
+        <td>${propiedades.operacion}</td>
+        <td>${propiedades.promocion}</td>
+        <td> <span class="material-symbols-outlined">delete</span></td>
+        <td><span class="material-symbols-outlined">edit</span></td>
+      </tr>
+       `
+  });
+
+  code = code + "</table>";
+  tablaDePropiedades.innerHTML = code;
+
+}
+
+
+function redireccionar() {
+  // Cambia la URL a la que deseas redireccionar
+  window.location.href("pages/admin.html");
+}
+
+function cargaPropiedadesPromocionadas() {
+  let code = ``;
+  propiedadesArray.forEach(function (propiedadesUnicas) {
+
+    // Solo cargamos las propiedades promocionadas
+    if (propiedadesUnicas.promocion == 'si') {
+      code = code +
+        `
+<div class="box">
+                <div class="top">
+                  <img src="${propiedadesUnicas.img}" alt="" />
+                  <span
+                    ><i class="fas fa-heart"></i><i class="fas fa-exchange-alt"></i
+                  ></span>
+                </div>
+                <div class="bottom">
+                  <h3>${propiedadesUnicas.title}</h3>
+                  <p> ${propiedadesUnicas.descripcion}
+                  </p>
+                  <div class="advants">
+                    <div>
+                      <span>Bedrooms</span>
+                      <div><i class="fas fa-th-large"></i><span>${propiedadesUnicas.bedrooms}</span></div>
+                    </div>
+                    <div>
+                      <span>Bathrooms</span>
+                      <div><i class="fas fa-shower"></i><span>${propiedadesUnicas.bathrooms}</span></div>
+                    </div>
+                    <div>
+                      <span>Area</span>
+                      <div>
+                        <i class="fas fa-vector-square"></i
+                        ><span>${propiedadesUnicas.area}<span>Sq Ft</span></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="price">
+                    <span>${propiedadesUnicas.type}</span>
+                    <span>${propiedadesUnicas.price}</span>
+                  </div>
+                </div>
+              </div>`
+
     } else {
-        alert(" Muchas gracias por contactarte. Que tengas buen dia ")
+
     }
+  });
+
+  propiedadesCardsPrincipal.innerHTML = code;
 }
 
-function logeo(apellidoUsuario) { //# sourceURL=factorialize.js (lo uso para debuggear)
+function buscarPropiedades(e) {
+  let tipoOperacion = formularioBusquedaPropiedades.tipo_operacion.value;
+  let tipoPropiedad = formularioBusquedaPropiedades.tipo_propiedad.value;
+  let precioMinimo = parseInt(formularioBusquedaPropiedades.precio_minimo.value);
+  let precioMaximo = parseInt(formularioBusquedaPropiedades.precio_maximo.value);
+  cargaPropiedadesBuscadas(tipoOperacion, tipoPropiedad, precioMinimo, precioMaximo);
+}
 
-    let apellidoLogeo = prompt("Ingresa tu apellido para ingresar por favor ( Debe ser el mismo apellido que ingresaste previamente ) ");
+// Cargamos las propiedades buscadas por el usuario
+function cargaPropiedadesBuscadas(operacion, tipo, precioMin, precioMax) {
+  alert(operacion + tipo + precioMin + precioMax);
 
-    if (apellidoLogeo.trim() == apellidoUsuario.trim()) {
-        let contrasena = prompt("El apellido es correcto. Por favor ingresa la contraseña, la correcta es: 1234");
+  let code = ``;
+  propiedadesArray.forEach(function (propiedadesBuscadas) {
 
-        if (contrasena.trim() == '1234') {
-            alert("La contraseña es correcta - Bienvenido a nuestra inmobiliaria");
-            barrios();
-        } else {
-            alert("La Contraseña ingresada es incorrecta volve a empezar el proceso por favor");
-            saludar();
-        }
+    if (propiedadesBuscadas.operacion == operacion && propiedadesBuscadas.type == tipo && propiedadesBuscadas.price >= precioMin
+      && propiedadesBuscadas.price <= precioMax) {
+
+      code = code +
+        `
+      <div class="box">
+                <div class="top">
+                  <img src="${propiedadesBuscadas.img}" alt="" />
+                  <span
+                    ><i class="fas fa-heart"></i><i class="fas fa-exchange-alt"></i
+                  ></span>
+                </div>
+                <div class="bottom">
+                  <h3>${propiedadesBuscadas.title}</h3>
+                  <p> ${propiedadesBuscadas.descripcion}
+                  </p>
+                  <div class="advants">
+                    <div>
+                      <span>Bedrooms</span>
+                      <div><i class="fas fa-th-large"></i><span>${propiedadesBuscadas.bedrooms}</span></div>
+                    </div>
+                    <div>
+                      <span>Bathrooms</span>
+                      <div><i class="fas fa-shower"></i><span>${propiedadesBuscadas.bathrooms}</span></div>
+                    </div>
+                    <div>
+                      <span>Area</span>
+                      <div>
+                        <i class="fas fa-vector-square"></i
+                        ><span>${propiedadesBuscadas.area}<span>Sq Ft</span></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="price">
+                    <span>${propiedadesBuscadas.type}</span>
+                    <span>${propiedadesBuscadas.price}</span>
+                  </div>
+                </div>
+              </div>`
+
     } else {
-        alert("El apellido es incorrecto volve a empezar el proceso  por favor");
-        saludar();
+
     }
+  });
+
+  alert(code)
+  propiedadesCardsPrincipal.innerHTML = code;
 }
 
-function barrios() {
-    let cantidad = 0;
-    let barrios = [];
-    alert(" Ingresa los nombres de los barrios en que estes interesado asi podemos notificarte nuestras ofertas");
-    let seguir = 's';
 
-    while (seguir.trim() == 's' || seguir.trim() == 'S') {
-        let barrio = prompt('Ingresa un barrio de tu interes');
-        if (barrio != "") {
-            barrios.push(barrio);
-            cantidad++;
-        } else {
-            alert(" los barrios tienen que tener al menos un caracter ");
-        }
+// Carga solo ultimas propiedades 
+function cargaUltimasPropiedades() {
 
-        seguir = prompt('Desea seguir(s/n)');
-    }
+  let code = ``;
 
-    mostrarBarrios(barrios);
+  for (let i = 0; i <= 3; i++) {
+
+    code = code +
+      `
+      <div class="box">
+                      <div class="top">
+                        <img src="${propiedadesArray[i].img}" alt="" />
+                        <span
+                          ><i class="fas fa-heart"></i><i class="fas fa-exchange-alt"></i
+                        ></span>
+                      </div>
+                      <div class="bottom">
+                        <h3>${propiedadesArray[i].title}</h3>
+                        <p> ${propiedadesArray[i].descripcion}
+                        </p>
+                        <div class="advants">
+                          <div>
+                            <span>Bedrooms</span>
+                            <div><i class="fas fa-th-large"></i><span>${propiedadesArray[i].bedrooms}</span></div>
+                          </div>
+                          <div>
+                            <span>Bathrooms</span>
+                            <div><i class="fas fa-shower"></i><span>${propiedadesArray[i].bathrooms}</span></div>
+                          </div>
+                          <div>
+                            <span>Area</span>
+                            <div>
+                              <i class="fas fa-vector-square"></i
+                              ><span>${propiedadesArray[i].area}<span>Sq Ft</span></span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="price">
+                          <span>${propiedadesArray[i].type}</span>
+                          <span>${propiedadesArray[i].price}</span>
+                        </div>
+                      </div>
+                    </div>`
+  }
+
+  propiedadesCardsUltimosIngresos.innerHTML = code;
 }
 
-function mostrarBarrios(barrios) {
 
-    let cantidadBarrios = barrios.length;
+function cargarOpcionesBusquedaTest() {
 
-    if (cantidadBarrios > 0) {
-        // si eligio algun/os barrios entra aca
-        alert(" Has elegido: " + cantidadBarrios + " barrios")
+  arrayTipoPropiedad.sort();
+  addOpcionesPropiedad("tipo_propiedad", arrayTipoPropiedad);
 
-        for (let i = 0; i < cantidadBarrios; i++) {
-            // Entró aca porque la cantidad de barrios elegidos es > 0. Si eligio un solo barrio entra aca ( el mensaje es distinto )
-            if (cantidadBarrios == 1) {
-                alert("Haz elegido un solo barrio y ese es :" + barrios[i]);
-            } else {
-                // si eligio mas de un  barrio entra aca ( los mensajes son distintos para el ultimo y el resto)
-                if (i != cantidadBarrios - 1) {
-                    alert("El barrio de: " + barrios[i]);
-                } else {
-                    alert("Y por ultimo el barrio de: " + barrios[i]);
-                    alert("muchas gracias por elegirnos");
-                }
-            }
-        }
-    } else {
-        // si NO eligio barrios entra aca
-        alert("No has elegido ningun barrio - Que tengas buen dia")
-    }
+  arrayTipoPropiedad.sort();
+  addOpcionesTipoOperacion("tipo_operacion", arrayTipoOperacion);
+
+  arrayPrecioMinimo.sort();
+  addOpcionesPrecioMinimo("precio_minimo", arrayPrecioMinimo);
+
+  arrayPrecioMaximo.sort();
+  addOpcionesPrecioMaximo("precio_maximo", arrayPrecioMaximo);
 }
+
+
+// Muestra el Modal 
+function mostrarModal() {
+
+  if (sessionStorage.getItem("mostrarModal") != 'true') {
+    sessionStorage.setItem("mostrarModal", true);
+    document.getElementById("modal").className = "modal__show";
+  }
+}
+
+// Cierra el modal
+function cerrarModal() {
+  document.getElementById("modal").className = "modal";
+}
+
+
+
+
+
+//Cargar opciones en los campos <select>
+function cargarOpcionesBusqueda() {
+
+  arrayTipoPropiedad.sort();
+  addOpcionesPropiedad("tipo_propiedad", arrayTipoPropiedad);
+
+  arrayTipoPropiedad.sort();
+  addOpcionesTipoOperacion("tipo_operacion", arrayTipoOperacion);
+
+  arrayPrecioMinimo.sort();
+  addOpcionesPrecioMinimo("precio_minimo", arrayPrecioMinimo);
+
+  arrayPrecioMaximo.sort();
+  addOpcionesPrecioMaximo("precio_maximo", arrayPrecioMaximo);
+}
+
+
+function addOpcionesPropiedad(domElement, arrayTipoPropiedad) {
+  var select = document.getElementsByName(domElement)[0];
+
+  for (value in arrayTipoPropiedad) {
+    var option = document.createElement("option");
+    option.text = arrayTipoPropiedad[value];
+    select.add(option);
+  }
+}
+
+function addOpcionesTipoOperacion(domElement, arrayTipoOperacion) {
+  var select = document.getElementsByName(domElement)[0];
+
+  for (value in arrayTipoOperacion) {
+    var option = document.createElement("option");
+    option.text = arrayTipoOperacion[value];
+    select.add(option);
+  }
+}
+
+function addOpcionesPrecioMinimo(domElement, arrayPrecioMinimo) {
+  var select = document.getElementsByName(domElement)[0];
+
+  for (value in arrayPrecioMinimo) {
+    var option = document.createElement("option");
+    option.text = arrayPrecioMinimo[value];
+    select.add(option);
+  }
+}
+
+function addOpcionesPrecioMaximo(domElement, arrayPrecioMaximo) {
+  var select = document.getElementsByName(domElement)[0];
+
+  for (value in arrayPrecioMaximo) {
+    var option = document.createElement("option");
+    option.text = arrayPrecioMaximo[value];
+    select.add(option);
+  }
+}
+
+
+
+
+
+
 
---
+++ js/datos.js
@@ -0,0 +1,200 @@
+const arrayTipoPropiedad = ["Casa", "Departamento", "PH"];
+const arrayTipoOperacion = ["Venta", "Alquiler"];
+const arrayPrecioMinimo= [0, 10000, 50000, 100000,500000];
+const arrayPrecioMaximo= [0, 10000, 50000, 100000,500000,1000000,5000000];
+
+const propiedadesArray = [
+    {
+        img: "img/casas/casa2.jpg",
+        title: "Casa con cochera",
+        descripcion: "Hermosa casa en barrio de Villa Devoto",
+        bedrooms: "4",
+        bathrooms: "2",
+        area: 20,
+        price: 310000,
+        type: "Casa",
+        zone: "devoto",
+        operacion:"Venta",
+        fecha: new Date("2023-6-15"),
+        promocion:"si"
+    },
+    {
+        img: "img/casas/casa3.jpg",
+        title: "Vivienda familiar - 3 AMB",
+        descripcion: "Vivienda urbana moderna, recientemente reformada",
+        bedrooms: "2",
+        bathrooms: "1",
+        area: 120,
+        price: 122000,
+        type: "Casa",
+        zone: "devoto",
+        operacion:"Venta",
+        fecha:new Date("2023-6-15"),
+        promocion:"si"
+    },
+    {
+        img: "img/casas/casa4.jpg",
+        title: "Casa  Reciclada",
+        descripcion: "Vivienda familiar en barrio residencial muy solicitado",
+        bedrooms: "3",
+        bathrooms: "2",
+        area: 150,
+        price: 150000,
+        type: "Casa",
+        zone: "devoto",
+        operacion:"Venta",
+        fecha:new Date("2023-6-15"),
+        promocion:"si"
+    },
+    {
+        img: "img/casas/casa5.jpg",        
+        title: "Residencia de Lujo",
+        descripcion: "Residencia de Lujo con cochera y pileta",
+        bedrooms: "2",
+        bathrooms: "4",
+        area: 280,
+        price: 340000,
+        type: "Casa",
+        zone: "paternal",
+        operacion:"Venta",
+        fecha:new Date("2022-1-15"),
+        promocion:"si"
+    },
+    {
+        img: "img/casas/casa6.jpg",        
+        title: "Lujosa propiedad",
+        descripcion: "En el centro del barrio de devoto",
+        bedrooms: "2",
+        bathrooms: "12",
+        area: 250,
+        price: 250.000,
+        type: "Casa",
+        zone: "devoto",
+        operacion:"Venta",
+        fecha:new Date("2022-1-15"),
+        promocion:"si"
+    },
+    {
+        img: "img/casas/casa1.jpg",
+        title: "Amplio casa de Barrio",
+        descripcion: "Casa con 3 habitaciones, 2 baños, cocina integral, terraza con excelente vista ",
+        bedrooms: "2",
+        bathrooms: "2",
+        area: 250,
+        price: 120000,
+        type: "Casa",
+        zone: "Villa del Parque",
+        operacion:"Alquiler",
+        fecha:new Date("2022-1-15"),
+        promocion:"si"
+    },
+    {
+        img: "img/departamentos/dto1.jpg",
+        title: "heroso departamento con cochera",
+        descripcion: "Cercana a colegios, parques, hospitales",
+        bedrooms: "2",
+        bathrooms: "12",
+        area: 80,
+        price: 110000,
+        type: "Departamento",
+        zone: "villa del Parque",
+        operacion:"Alquiler",
+        fecha:new Date("2022-1-15"),
+        promocion:"no"
+    },
+    {
+        img: "img/departamentos/dto2.jpg",
+        title: "herosa casa con cochera",
+        descripcion: "casa en barrio de devoto...",
+        bedrooms: "2",
+        bathrooms: "1",
+        area: 120,
+        price: 190000,
+        type: "Departamento ",
+        zone: "devoto",
+        operacion:"Alquiler",
+        fecha:new Date("2023-1-15"),
+        promocion:"no"
+    },
+    {
+        img: "img/departamentos/dto3.jpg",
+        title: "Departamenro a estrenar",
+        descripcion: "apartamento para estrenar , luminoso",
+        bedrooms: "2",
+        bathrooms: "1",
+        area: 250,
+        price: 150000,
+        type: "Departamento",
+        zone: "Paternal",
+        operacion:"Alquiler",
+        fecha:new Date("2023-1-15"),
+        promocion:"no"
+    },
+    {
+        img: "img/departamentos/dto4.jpg",
+        title: "Departamento como nuevo",
+        descripcion: "departamento  de 3 habitaciones que goza de increíbles vistas sobre la ciudad",
+        bedrooms: "2",
+        bathrooms: "12",
+        area: 50,
+        price: 350000,
+        type: "Casa",
+        zone: "villa del parque",
+        operacion:"Alquiler",
+        fecha:new Date("2013-1-15"),
+        promocion:"no"
+    },
+    {
+        img: "img/departamentos/dto5.jpg",
+        title: "4 Ambientes con cochera",
+        descripcion: "Se vende un 7º piso recién reformado con 3 habitaciones y garaje",
+        bedrooms: "3",
+        bathrooms: "2",
+        area: 130,
+        price: 300000,
+        type: "Departamento",
+        zone: "devoto",
+        operacion:"Venta",
+        fecha:new Date("2010-1-15"),
+        promocion:"no"
+    },
+    {
+        img: "img/ph/ph1.webp",
+        title: "Ph 3 Ambientes",
+        descripcion: "Ph 3 Amb · Terraza Propia",
+        bedrooms: "2",
+        bathrooms: "1",
+        area: 90,
+        price: 300000,
+        type: "PH",
+        zone: "devoto",
+        operacion:"Venta",
+        fecha:new Date("2010-1-15"),
+        promocion:"no"
+    },
+    {
+        img: "img/departamentos/dto5.jpg",
+        title: "Ph 4 Ambientes",
+        descripcion: "Ph 4 Ambientes - Jardín Al Frente Y Al Fondo",
+        bedrooms: "3",
+        bathrooms: "2",
+        area: 120,
+        price: 170000,
+        type: "PH",
+        zone: "villa del parque",
+        operacion:"Venta",
+        fecha:new Date("2010-1-15"),
+        promocion:"no"
+    }
+]
+
+let arrayPropiedadesOrdenadasFecha = propiedadesArray.map(function(objeto) {
+    return { ...objeto };
+  });
+
+  
+  arrayPropiedadesOrdenadasFecha.sort(function(a, b) {
+    var fechaA = a.fecha;
+    var fechaB = b.fecha;
+    return fechaB - fechaA;
+  });
